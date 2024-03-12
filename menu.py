a71345d3d151287e309a5aa3be78bba974a0d395
jgonzalezal37 <jgonzalezal37@gmail.com>
2024-03-12 12:42:39 +0100

    menu creado

--- menu.py
+++ menu.py
@@ -1,35 +1,12 @@
 import tkinter as tk
-
 class MenuApp:
-    def __init__(self, root):
-        self.root = root
-        self.create_menu()
+    def __init__(self):
+        
+        self.menu = tk.Toplevel()
+        self.menu.geometry("450x450")
+        self.menu.title("Menú principal")
+        self.menu.iconbitmap("Icons/A.ico")
+        self.menu.resizable(0, 0)
 
-    def do_something(self):
-        print("Se ha seleccionado una opción del menú.")
-
-    def exit_app(self):
-        self.root.destroy()
-
-    def create_menu(self):
-        # Crear el menú
-        menu_bar = tk.Menu(self.root)
-
-        # Menú Archivo
-        file_menu = tk.Menu(menu_bar, tearoff=0)
-        file_menu.add_command(label="Abrir", command=self.do_something)
-        file_menu.add_command(label="Guardar", command=self.do_something)
-        file_menu.add_separator()
-        file_menu.add_command(label="Salir", command=self.exit_app)
-        menu_bar.add_cascade(label="Archivo", menu=file_menu)
-
-        # Menú Ayuda
-        help_menu = tk.Menu(menu_bar, tearoff=0)
-        help_menu.add_command(label="Ayuda", command=self.do_something)
-        help_menu.add_separator()
-        help_menu.add_command(label="Acerca de", command=self.do_something)
-        menu_bar.add_cascade(label="Ayuda", menu=help_menu)
-
-        # Configurar la ventana principal para usar el menú
-        self.root.config(menu=menu_bar)
-
+        
+       
