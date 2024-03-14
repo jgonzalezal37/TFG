bee292da2b608d70ecb01534118254d9217dd020
jgonzalezal37 <jgonzalezal37@gmail.com>
2024-03-14 11:05:26 +0100

    menu

--- menu.py
+++ menu.py
@@ -1,12 +1,17 @@
 import tkinter as tk
+
 class MenuApp:
     def __init__(self):
-        
         self.menu = tk.Toplevel()
         self.menu.geometry("450x450")
         self.menu.title("Menú principal")
-        self.menu.iconbitmap("Icons/A.ico")
         self.menu.resizable(0, 0)
-
+        self.menu.iconbitmap("Icons/A.ico")
+        self.image = tk.PhotoImage(file="Images/slice_game.png")
+        #self.image2 = tk.PhotoImage(file="Images/slice_game.png")
+        self.image_label = tk.Label(self.menu, image=self.image)
+        #self.image_label2 = tk.Label(self.menu, image=self.image2)
+        self.image_label.place(x=0,y=0,width=450,height=100)
+        #self.image_label2.place(x=0,y=100,width=450,height=100)
         
-       
+        self.menu.mainloop()
