b6bc72d9d224a28d224e8a3ac7d3e51e27e78506
jgonzalezal37 <jgonzalezal37@gmail.com>
2024-03-20 13:41:30 +0100

    menu actualizado con niveles facil y medio

--- menu.py
+++ menu.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import PhotoImage
 import os
-from juego import *
+from modoFacil import modoPrincipiante
+from modoMedio import modoMedio
 
 class MenuApp:
     def __init__(self):
@@ -11,39 +12,34 @@ class MenuApp:
         self.menu.resizable(0, 0)
         
         # Cargar las imágenes para cada botón
-        self.image = PhotoImage(file="Images/menu.gif")  # Cambia la ruta de la imagen MENU
-        self.image1 = PhotoImage(file="Images/principiante.png")  # Cambia la ruta de la primera imagen
-        self.image2 = PhotoImage(file="Images/medio.png")  # Cambia la ruta de la segunda imagen
-        self.image3 = PhotoImage(file="Images/dificil.png")  # Cambia la ruta de la tercera imagen
+        self.image = PhotoImage(file="Images/menu.gif")
+        self.image1 = PhotoImage(file="Images/principiante.png")
+        self.image2 = PhotoImage(file="Images/medio.png")
+        self.image3 = PhotoImage(file="Images/dificil.png")
 
-        def modoFacil(event):
-            modoFacil = modoPrincipiante()
-        
         # Crear un widget Label para mostrar la imagen MENU
         self.image_label = tk.Label(self.menu, image=self.image)
-        self.image_label.pack()  # Empaqueta el widget en la ventana
+        self.image_label.pack()
 
         # Añadir los botones con las imágenes correspondientes
-        self.principante = tk.Label(self.menu, image=self.image1)
-        self.principante.place(x=0, y=100, width=450, height=100)
-        self.principante.bind("<Button-1>",modoFacil)
+        self.principiante = tk.Label(self.menu, image=self.image1)
+        self.principiante.place(x=0, y=100, width=450, height=100)
+        self.principiante.bind("<Button-1>", self.modoFacil)
 
-        self.image_label = tk.Label(self.menu, image=self.image2)
-        self.image_label.place(x=0, y=200, width=450, height=100)
+        self.medio = tk.Label(self.menu, image=self.image2)
+        self.medio.place(x=0, y=200, width=450, height=100)
+        self.medio.bind("<Button-1>", self.nivelMedio)
 
         self.image_label = tk.Label(self.menu, image=self.image3)
         self.image_label.place(x=0, y=300, width=450, height=100)
 
-        # Se pueden añadir más elementos o funcionalidades aquí
-        
         self.menu.mainloop()
 
-    def on_button1_click(self):
-        print("Se hizo clic en el botón 1")
+    def modoFacil(self, event):
+        modoFacil = modoPrincipiante()
+        modoFacil.run()
 
-    def on_button2_click(self):
-        print("Se hizo clic en el botón 2")
+    def nivelMedio(self, event):
+        nivelMedio = modoMedio()
+        nivelMedio.run()
 
-    def on_button3_click(self):
-        print("Se hizo clic en el botón 3")
-    
